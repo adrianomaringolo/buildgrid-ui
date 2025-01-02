41062872c2807cfa14494c9cab1f2b8fe506d246
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2025-01-02 17:14:38 -0300

    feat: add Badge component and related stories

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-import type { Meta, StoryObj } from "@storybook/react";
-
-import { Button, ButtonProps } from "./Button";
+// organize-imports-ignore
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from './Button'
 
 const meta: Meta<typeof Button> = {
-  component: Button,
-};
+	component: Button,
+}
 
-export default meta;
-type Story = StoryObj<typeof Button>;
+export default meta
+type Story = StoryObj<typeof Button>
 
 const Template = (args: ButtonProps) => {
-  return (
-    <div style={{ padding: "10px" }}>
-      <Button>{args.children}</Button>
-    </div>
-  );
-};
+	return (
+		<div style={{ padding: '10px' }}>
+			<Button>{args.children}</Button>
+		</div>
+	)
+}
 
 export const Default: Story = {
-  render: Template.bind({}),
-  args: {
-    children: "Primary Button",
-    variant: "default",
-    size: "default",
-  },
-};
+	render: Template.bind({}),
+	args: {
+		children: 'Primary Button',
+		variant: 'default',
+		size: 'default',
+	},
+}
