bbc4e9a54e3ea295622c8598d51576f3cddd1b3d
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2025-01-01 22:18:44 -0300

    feat: add Button component with Storybook integration and initial configuration

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "BuildgridUI",
+      fileName: (format) => `buildgrid-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
