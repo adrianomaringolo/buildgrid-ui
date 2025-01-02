69292b3d467dd5ed8eacf521e9c5f89e465af323
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2025-01-02 15:21:47 -0300

    feat: enhance Button component stories with props and update export

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from "./Button";
+import { Button, ButtonProps } from "./Button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
@@ -9,8 +9,19 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
+const Template = (args: ButtonProps) => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <Button>{args.children}</Button>
+    </div>
+  );
+};
+
+export const Default: Story = {
+  render: Template.bind({}),
   args: {
-    label: "Button example",
+    children: "Primary Button",
+    variant: "default",
+    size: "default",
   },
 };
