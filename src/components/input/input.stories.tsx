8078ee7c0e9b986007d89096d08065300c36b7c9
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2025-01-02 23:46:48 -0300

    feat(input): add Input component with stories

---
+++ src/components/input/input.stories.tsx
@@ -0,0 +1,22 @@
+// organize-imports-ignore
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Input } from './input'
+
+const meta: Meta<typeof Input> = {
+	title: 'Components/Input/Simple',
+	component: Input,
+}
+
+export default meta
+type Story = StoryObj<typeof Input>
+
+const Template = () => {
+	return <Input className="w-64" />
+}
+
+export const Default: Story = {
+	render: Template.bind({}),
+	args: {},
+}
