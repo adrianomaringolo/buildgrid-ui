2c12b28446d972ba3719264b14e62c2ac961f52d
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2025-01-05 01:09:41 -0300

    feat(textarea): add textarea component and story

---
+++ src/components/textarea/textarea.stories.tsx
@@ -0,0 +1,21 @@
+// organize-imports-ignore
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Textarea } from './textarea'
+
+const meta: Meta<typeof Textarea> = {
+	component: Textarea,
+}
+
+export default meta
+type Story = StoryObj<typeof Textarea>
+
+const Template = () => {
+	return <Textarea className="w-64" />
+}
+
+export const Default: Story = {
+	render: Template.bind({}),
+	args: {},
+}
