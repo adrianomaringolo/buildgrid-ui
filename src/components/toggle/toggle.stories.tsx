f120713db932e5bb7af6c68785700192e5aecbcd
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2025-01-07 00:07:33 -0300

    feat(toggle): add toggle component ans stories

---
+++ src/components/toggle/toggle.stories.tsx
@@ -0,0 +1,27 @@
+// organize-imports-ignore
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Toggle } from './toggle'
+
+const meta: Meta<typeof Toggle> = {
+	component: Toggle,
+}
+
+export default meta
+type Story = StoryObj<typeof Toggle>
+
+const Template = () => {
+	return (
+		<>
+			<Toggle>A</Toggle>
+			<Toggle>B</Toggle>
+			<Toggle>C</Toggle>
+		</>
+	)
+}
+
+export const Default: Story = {
+	render: Template.bind({}),
+	args: {},
+}
