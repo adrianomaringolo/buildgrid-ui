d1d0c9506084dcf7db632c057c6b7f25b2da438f
adrianomaringolo <adrianomaringolo@users.noreply.github.com>
2025-01-16 16:57:05 +0000

    deploy: 58067e08cadc791048bc470b2da5ddbee02d9004

---
+++ assets/js/acecf23e.bba6a656.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/buildgrid-ui/blog","blogTitle":"Blog","authorsListPath":"/buildgrid-ui/blog/authors"}')}}]);
