d1efdb5a1a9de86ed05cd2777e78030e4860aade
Adriano Maringolo <adrianomaringolo@gmail.com>
2025-06-02 16:57:50 -0300

    fix: update Skeleton component to use shimmer animation for loading state

--- src/components/skeleton/skeleton.tsx
+++ src/components/skeleton/skeleton.tsx
@@ -10,7 +10,10 @@ export const Skeleton = ({ className, repeat = 1, ...props }: SkeletonProps) => 
 			{Array.from({ length: repeat ?? 1 }).map((_, index) => (
 				<div
 					key={index}
-					className={cn('animate-pulse rounded-md bg-gray-700/10 w-full h-5', className)}
+					className={cn(
+						'animate-shimmer bg-gradient-to-r from-gray-400 via-gray-200 to-gray-400 bg-[length:200%_100%] rounded-md h-5',
+						className,
+					)}
 					{...props}
 				/>
 			))}
