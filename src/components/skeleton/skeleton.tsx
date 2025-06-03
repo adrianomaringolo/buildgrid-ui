420fdc4831ac4a33f9365a077b3c8de95de3e182
Adriano Maringolo <adrianomaringolo@gmail.com>
2025-06-02 22:51:53 -0300

    chore: update Skeleton component colors for improved visibility

--- src/components/skeleton/skeleton.tsx
+++ src/components/skeleton/skeleton.tsx
@@ -11,7 +11,7 @@ export const Skeleton = ({ className, repeat = 1, ...props }: SkeletonProps) => 
 				<div
 					key={index}
 					className={cn(
-						'animate-shimmer bg-gradient-to-r from-gray-400 via-gray-200 to-gray-400 bg-[length:200%_100%] rounded-md h-5',
+						'animate-shimmer bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 bg-[length:200%_100%] rounded-md h-5',
 						className,
 					)}
 					{...props}
