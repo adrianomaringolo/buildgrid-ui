cf56c11541955cbde61d95ac10d05b3eeefec0d8
Adriano Maringolo <adrianomaringolo@gmail.com>
2025-07-03 00:10:38 -0300

    fix: export options

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,17 @@
-import type { StorybookConfig } from '@storybook/react-vite'
-import { mergeConfig } from 'vite'
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: [
-		'@storybook/addon-onboarding',
-		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
-		'@storybook/addon-interactions',
-		'@etchteam/storybook-addon-status',
-	],
-	framework: {
-		name: '@storybook/react-vite',
-		options: {},
-	},
-	async viteFinal(config) {
-		return mergeConfig(config, {
-			css: {
-				postcss: {
-					plugins: [require('tailwindcss'), require('autoprefixer')],
-				},
-			},
-		})
-	},
-}
-export default config
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
