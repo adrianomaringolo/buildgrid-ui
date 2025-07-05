6c192bfb7f011359fe4498d5c110198a3600837a
Adriano Maringolo <adrianomaringolo@gmail.com>
2025-07-04 22:49:07 -0300

    fix: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,58 +1,44 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import { globSync } from "glob";
-import { peerDependencies, dependencies } from "./package.json";
-
-const external = [
-  ...Object.keys(peerDependencies),
-  ...Object.keys(dependencies),
-];
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react(), dts({ outDir: "dist" })],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.ts",
-    coverage: {
-      provider: "c8",
-      reporter: ["text", "lcov"],
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
-    },
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      external,
-      input: Object.fromEntries(
-        globSync("src/**/*.{ts,tsx}").map((file) => [
-          path.relative(
-            "src",
-            file.slice(0, file.length - path.extname(file).length)
-          ),
-          file,
-        ])
-      ),
-      output: {
-        format: "es",
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
-      },
-    },
-  },
-});
+	plugins: [react(), dts({ outDir: 'dist', insertTypesEntry: true })],
+	test: {
+		globals: true, // Use global APIs like `describe` and `it`
+		environment: 'jsdom', // Simulates the browser environment
+		setupFiles: './vitest.setup.ts', // Path to setup file
+		coverage: {
+			provider: 'c8', // Built-in coverage tool
+			reporter: ['text', 'lcov'], // Coverage formats
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	css: {
+		postcss: {
+			plugins: [require('tailwindcss'), require('autoprefixer')],
+		},
+	},
+
+	build: {
+		lib: {
+			entry: 'src/index.ts',
+			name: 'BuildgridUI',
+			fileName: (format) => `buildgrid-ui.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
+})
